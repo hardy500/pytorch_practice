2c9d2acd097aabf294c00100c99097f689c5423a
toto_wng <r.news94@gmail.com>
2023-01-09 20:27:55 +0100

    add simple resnet

---
+++ c4_cnn/w2/simple_resnet.py
@@ -0,0 +1,148 @@
+import time
+import numpy as np
+import torch
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision import transforms
+from torch import nn
+
+### Settings ###
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# Hyperparameters
+RANDOM_SEED = 0
+LR          = 0.01
+EPOCHS      = 10
+BS          = 128
+
+# Architecture
+N_CLASSES = 10
+
+
+### MNIST DATASET ###
+train_dataset = datasets.MNIST(root='data',
+                               train=True,
+                               transform=transforms.ToTensor(),
+                               download=True)
+
+test_dataset = datasets.MNIST(root='data',
+                              train=False,
+                              transform=transforms.ToTensor(),
+                              download=True)
+
+train_loader = DataLoader(dataset=train_dataset,
+                          batch_size=BS,
+                          shuffle=True)
+
+test_loader = DataLoader(dataset=test_dataset,
+                         batch_size=BS,
+                         shuffle=False)
+
+# Checking the dataset
+# torch.Size([128, 1, 28, 28]) torch.Size([128])
+#for x, y in train_loader:
+#  print(x.shape, y.shape)
+#  break
+
+### Model ###
+
+class ConvNet(nn.Module):
+  def __init__(self, n_classes):
+    super().__init__()
+
+    # First residual block
+    self.block_1 = nn.Sequential(
+        nn.Conv2d(in_channels=1,
+                  out_channels=4,
+                  kernel_size=1,
+                  stride=1,
+                  padding=0),
+
+        nn.BatchNorm2d(4),
+        nn.ReLU(),
+
+        nn.Conv2d(in_channels=4,
+                  out_channels=1,
+                  kernel_size=3,
+                  stride=1,
+                  padding=1),
+
+        nn.BatchNorm2d(1)
+    )
+
+    self.block_2 = nn.Sequential(
+        nn.Conv2d(in_channels=1,
+                  out_channels=4,
+                  kernel_size=1,
+                  stride=1,
+                  padding=0),
+
+        nn.BatchNorm2d(4),
+        nn.ReLU(),
+
+        nn.Conv2d(in_channels=4,
+                  out_channels=1,
+                  kernel_size=3,
+                  stride=1,
+                  padding=1),
+
+        nn.BatchNorm2d(1)
+    )
+
+    # Fully connected
+    self.linear_1 = nn.Linear(1*28*28, n_classes)
+
+  def forward(self, x):
+    # First residual block
+    shortcut = x
+    x = self.block_1(x)
+    x = nn.functional.relu(x + shortcut)
+
+    # Second residual block
+    shortcut = x
+    x = self.block_2(x)
+    x = nn.functional.relu(x + shortcut)
+
+    # Fully connect
+    logits = self.linear_1(x.view(-1, 1*28*28))
+    return logits
+
+def accuracy(mode, data_loader):
+  correct_pred, num_examples = 0, 0
+  for i, (x, y) in enumerate(data_loader):
+    x, y = x.to(DEVICE), y.to(DEVICE)
+
+    logits = model(x)
+    _, pred = torch.max(logits, 1)
+    num_examples += y.size(0)
+    correct_pred += (pred == y).sum()
+
+  return correct_pred.float()/num_examples
+
+
+torch.manual_seed(RANDOM_SEED)
+model = ConvNet(n_classes=N_CLASSES)
+model = model.to(DEVICE)
+optimizer = torch.optim.Adam(model.parameters(), lr=LR)
+
+start_time = time.time()
+for epoch in range(EPOCHS):
+  model = model.train()
+  for batch_idx, (x, y) in enumerate(train_loader):
+    x, y = x.to(DEVICE), y.to(DEVICE)
+
+    logits = model(x)
+    cost = nn.functional.cross_entropy(logits, y)
+    optimizer.zero_grad()
+
+    cost.backward()
+    optimizer.step()
+
+  model = model.eval()
+  with torch.inference_mode():
+    print('Epoch: %03d/%03d training accuracy: %.2f%%' % (epoch+1, EPOCHS, accuracy(model, train_loader)))
+
+  print("Time elapsed: %.2f min" % ((time.time() - start_time)/60))
+
+print("Total Trainint Time: %2.f min" % ((time.time() - start_time)/60))
+print("Test accuracy: %.2f" % (accuracy(model, test_loader)))
